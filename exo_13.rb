6dd83846a0edc9edc90403b578f37a0e6f7b5291
kevin <kevin@soucheyre.org>
2022-10-04 15:41:43 +0200

    third commit

--- exo_13.rb
+++ exo_13.rb
@@ -1,17 +1,14 @@
-number = 0
-tab = [loop do
-    number = number + 1
-    puts "jean.dupont.0#{number}@email.fr"
-    if number == 9
-        break
-    end
-end]
-number = 9
-tab = [loop do
-    number = number + 1
-    puts "jean.dupont.#{number}@email.fr"
-    if number == 50
-        break
-    end
-end]
+nums = Array.new()
+
+50.times do |i|
+    if i < 9
+    nums.push("jean.dupont.0#{i+1}@email.fr")
+    else
+    nums.push("jean.dupont.#{i+1}@email.fr")    
+end
+end
+
+puts nums
+
+
 
