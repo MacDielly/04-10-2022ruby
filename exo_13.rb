9b6c304103b8d356f43bc36d8a982aeb6366e2d8
kevin <kevin@soucheyre.org>
2022-10-04 14:35:02 +0200

    second commit

---
+++ exo_13.rb
@@ -0,0 +1,17 @@
+number = 0
+tab = [loop do
+    number = number + 1
+    puts "jean.dupont.0#{number}@email.fr"
+    if number == 9
+        break
+    end
+end]
+number = 9
+tab = [loop do
+    number = number + 1
+    puts "jean.dupont.#{number}@email.fr"
+    if number == 50
+        break
+    end
+end]
+
